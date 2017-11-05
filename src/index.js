dc03810341b14106e0816c99fd8721599e662f07
rcavezza <rcavezza@gmail.com>
2017-11-04 22:00:10 -0400

    Removes app.css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import './app.css'
 import './skeleton.css'
 import registerServiceWorker from './registerServiceWorker'
 import {
