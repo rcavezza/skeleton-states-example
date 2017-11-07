15bf37a01ba05fa225ed9ae90f53a493a9b419fd
rcavezza <rcavezza@gmail.com>
2017-11-07 07:37:15 -0500

    Removes package-lock.json from .gitignore file and removes double link to Voters in the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,10 +11,7 @@ const Navbar = () => {
       <div className="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item active">
-            <Link to={`/voters`} className="nav-link">Voters</Link>
+            <a className="nav-link" href="/">Voters <span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item active">
             <Link to={`/players`} className="nav-link">Players</Link>
